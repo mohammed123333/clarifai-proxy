a4369b3202cc67ef9aea39182d1a516c4a759d41
mohammed123333 <54069664+mohammed123333@users.noreply.github.com>
2025-06-04 18:08:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -74,3 +74,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
